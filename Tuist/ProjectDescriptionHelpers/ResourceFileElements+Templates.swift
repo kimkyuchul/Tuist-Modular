5ac45d1d84193b87141be11f541f2f69b7f47fa3
김규철 <rbcjf122@gmail.com>
2024-02-08 12:17:38 +0900

    [ADD] extension ResourceFileElements (#1)

---
+++ Tuist/ProjectDescriptionHelpers/ResourceFileElements+Templates.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceFileElements+Templates.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Kooky macBook Air on 2/8/24.
+//
+
+import ProjectDescription
+
+extension ResourceFileElements {
+    public static let `default`: ResourceFileElements = "Resources/**"
+}
